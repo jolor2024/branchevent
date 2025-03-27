1453a6b4f62b47239a14752bd9b3a55ac5dfb8a5
Jonatan <j.lorentzson@outlook.com>
2025-03-27 10:55:47 +0100

    add image

--- event-app/resources/js/pages/welcome.tsx
+++ event-app/resources/js/pages/welcome.tsx
@@ -10,8 +10,11 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <header className="mb-6 w-full text-sm not-has-[nav]:hidden bg-white">
-                <nav className="flex items-center justify-end gap-4 ">
+            <header className="w-full text-sm not-has-[nav]:hidden bg-white ">
+                <nav className="flex items-center justify-between gap-4 py-4 px-6 ">
+                    <a href="" className='w-44'>
+                        <img src="/yrgologo.svg"/>
+                    </a>
                     {auth.user ? (
                         <Link
                             href={route('dashboard')}
@@ -23,23 +26,25 @@ export default function Welcome() {
                         <>
                             <Link
                                 href={route('login')}
-                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035]"
+                                className="inline-block rounded-lg border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#19140035] bg-primary-red"
                             >
                                 Logga in
                             </Link>
-                            <Link
+                            {/*
+                                <Link
                                 href={route('register')}
                                 className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] "
                             >
                                 Register
                             </Link>
+                            */}
                         </>
                     )}
                 </nav>
+                <div className='bg-primary-red py-10'>
+                    <h1 className='text-[96px] text-white '>LIA <br /> EVENT <br />  2025 </h1>
+                </div>
             </header>
-            <main className="flex w-full flex-col-reverse lg:flex-row">
-                    <h1 className='bg-amber-300 text-4xl text-red-800'>Välkommen till yrgo branchevent</h1>
-            </main>
         </>
     );
 }
