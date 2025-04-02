dff9200a06391d17aa02d270fce512c5e81dd4c5
Luisal182 <luisal.arranz@hotmail.com>
2025-04-02 10:45:52 +0200

    Welcome site new changes
    Change in style and code for Tabs trigger and colors

--- event-app/resources/js/pages/welcome.tsx
+++ event-app/resources/js/pages/welcome.tsx
@@ -18,7 +18,7 @@ export default function Welcome() {
         const hero = document.getElementById('hero');
         const event = document.getElementById('event');
         const footer = document.getElementById('footer');
-
+        const registerButton = document.getElementById('register-button');
 
         if (activeTab === "company") {
             header.classList.remove("bg-primary-red");
@@ -27,6 +27,8 @@ export default function Welcome() {
             hero.classList.add("bg-primary-blue");
             footer.classList.remove("bg-primary-red");
             footer.classList.add("bg-primary-blue");
+            registerButton.classList.remove("bg-primary-red");
+            registerButton.classList.add("bg-primary-blue");
             event.innerHTML = "Här kan ni som företag registrera er och skapa en profil med snabb information, för att elever enklare ska kunna ta kontakt med er.";
         } else if (activeTab === "student") {
             header.classList.remove("bg-primary-blue");
@@ -35,6 +37,8 @@ export default function Welcome() {
             hero.classList.add("bg-primary-red");////
             footer.classList.remove("bg-primary-blue");
             footer.classList.add("bg-primary-red");
+            registerButton.classList.remove("bg-primary-blue");
+            registerButton.classList.add("bg-primary-red");
             event.innerHTML="Välkommen Digital Designers och Webbutvecklare på mingelevent för att hitta framtida LIA-platser och skapa kontakter med företag. ";
         }
     }, [activeTab]);
@@ -60,52 +64,76 @@ export default function Welcome() {
                   <CircleUserRound />
                     </Link>
 
-                </nav>
-                <div id="hero" className="bg-primary-blue py-10">
-                    <h1 className="text-[96px] text-white">
-                        LIA <br /> CONNECT
-                        <br /> 2025
-                    </h1>
-                </div>
+                   </nav>
+                   <div id="hero" className="bg-primary-blue py-10">
+                 <h1 className="text-white text-[68px] font-thin leading-[64px] tracking-tighter">
+                          LIA <br /> 
+                        <span className="text-[60px] font-bold">CONNECT</span><br /> 
+                        2025
+                            </h1>
+                               </div>
             </header>
                <main>
-                   <section>
-                   <Tabs defaultValue="account" value={activeTab} onValueChange={setActiveTab} className="w-[400px]">
-            <TabsList>
-                <TabsTrigger value="company">Företag</TabsTrigger>
-                <TabsTrigger value="student">ELEV</TabsTrigger>
-            </TabsList>
-            <TabsContent value="company">
-                <h2 className="text-2xl font-semibold mt-6 mb-2 text-right">FÖRETAG</h2>
-                <p className="mb-4 font-thin text-right">Här kan ni som företag registrera er och skapa en profil med snabb information, för att elever enklare ska kunna ta kontakt med er.</p>
-            </TabsContent>
-            <TabsContent value="student">
-                <h2 className="text-2xl font-semibold mt-6 mb-2 text-right">STUDENT</h2>
-                <p className="mb-4  font-thin text-right">Här kan du som elev registrera dig och skapa en profil för att enkelt oxh smidigt kunna hitta och ta kontakt med företag.</p>
-            </TabsContent>
-        </Tabs>       
-            </section>
+               <section>
+    <Tabs 
+        defaultValue="company" 
+        value={activeTab} 
+        onValueChange={setActiveTab} 
+        className="relative w-[344px] mt-[22px] ml-[25px]"
+    >
+        <TabsList className="flex h-[62px] rounded-[54px] bg-[#001A52] p-[7px_6px] justify-between">
+            <TabsTrigger 
+                value="company" 
+                className="rounded-full text-lg font-medium bg-white text-black"
+            >
+                FÖRETAG
+            </TabsTrigger>
+            <TabsTrigger 
+                value="student" 
+                className="rounded-full text-lg font-medium text-white"
+            >
+                ELEV
+            </TabsTrigger>
+        </TabsList>
+        <TabsContent value="company">
+            <h2 className="text-2xl font-semibold mt-6 mb-2 text-right">FÖRETAG</h2>
+            <p className="mb-4 font-thin text-right">Här kan ni som företag registrera er och skapa en profil med snabb information, för att elever enklare ska kunna ta kontakt med er.</p>
+        </TabsContent>
+        <TabsContent value="student">
+            <h2 className="text-2xl font-semibold mt-6 mb-2 text-right">STUDENT</h2>
+            <p className="mb-4 font-thin text-right">Här kan du som elev registrera dig och skapa en profil för att enkelt oxh smidigt kunna hitta och ta kontakt med företag.</p>
+        </TabsContent>
+    </Tabs>       
+</section>
             <section>
             <Link
                 href={route('register')}
-                className="bg-primary-blue inline-block rounded-lg border border-transparent px-5 py-1.5 text-sm leading-normal text-white hover:border-[#19140035]"
-            >
+                id="register-button"  className="bg-primary-blue inline-block rounded-[30px] border border-transparent px-[32px] py-[16px] text-sm leading-[16px] text-white hover:border-[#19140035] mb-4 w-[195px] h-[48px] font-medium uppercase tracking-normal"
+                >
                 Registrera dig
             </Link>
-            <h3 className="text-2xl font-semibold mt-6 mb-2">När och var. </h3>
+             {/* <h3 className="text-2xl font-semibold mt-6 mb-2">När och var. </h3>
                 <p className="mb-4">
                 23 april<br />
                     kl 13-15<br />
                     Lindholmspiren 3
-                </p>
-                <img src='/assets/wellcome-group.png' alt="welcome-group" />
+                </p>*/}
+                <img src='/assets/Smilley-colleagues.png' alt="welcome-group" className='mx-auto mt-4' />
                 </section>
                 <section>
                 <h4 className="text-2xl font-semibold mt-6 mb-2 ">OM EVENTET</h4>
-                <p id= "event"className="mb-4">Här kan ni som företag registrera er och skapa en profil med snabb information, för att elever enklare ska kunna ta kontakt med er.</p>
-                </section>
-            </main>
-            <FooterLayout/>
+                <p id="event" className="text-black text-lg font-normal leading-6 tracking-wide mb-4">
+                Välkomna på mingelevent för att hitta <br />
+                 framtida medarbetare i ert företag <br /> 
+                eller bara jobba tillsammans under<br /> 
+                 LIA. Ni kommer att träffa <br /> 
+                 Webbutvecklare och Digital Designers <br /> 
+                 från Yrgo som vill visa vad de har <br /> 
+                 jobbat med under året, och vi hoppas <br /> 
+                 att ni hittar en match. </p>     
+                      </section>
+                    </main> 
+            <FooterLayout/> 
         </>
     );
 }
